9eb8461da7107ae43c8c5eba5a4bf61e81753111
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 15:57:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,33 +3,4 @@
 let currentUser = "bob";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
-// welcomeMessage
-// contains "Welcome to Flatbook, " ‣
-// ReferenceError: welcomeMessage is not defined
-//     at Context.it (test/indexTest.js:13:14)
-// contains the value of the 'currentUser' variable ‣
-// ReferenceError: welcomeMessage is not defined
-//     at Context.it (test/indexTest.js:17:14)
-// ends with an exclamation point! ‣
-// ReferenceError: welcomeMessage is not defined
-//     at Context.it (test/indexTest.js:21:14)
-// excitedWelcomeMessage
-// contains "WELCOME TO FLATBOOK, " ‣
-// ReferenceError: excitedWelcomeMessage is not defined
-//     at Context.it (test/indexTest.js:27:14)
-// contains the value of the 'currentUser' variable ‣
-// ReferenceError: currentUser is not defined
-//     at Context.it (test/indexTest.js:31:36)
-// ends with an exclamation point ‣
-// ReferenceError: excitedWelcomeMessage is not defined
-//     at Context.it (test/indexTest.js:37:14)
-// shortGreeting
-// contains "Welcome, " ‣
-// ReferenceError: shortGreeting is not defined
-//     at Context.it (test/indexTest.js:43:14)
-// contains the first initial of the name stored in the 'currentUser' variable ‣
-// ReferenceError: currentUser is not defined
-//     at Context.it (test/indexTest.js:47:28)
-// ends with an exclamation point ‣
-// ReferenceError: shortGreeting is not defined
-//     at Context.it (test/indexTest.js:55:14)
+let shortGreeting = "Welcome, " + currentUser + "!";
